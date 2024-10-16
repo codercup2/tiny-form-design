3545d070b24222711c9dee8d86e5261da7749460
Burt <burt.lai@kupotech.com>
2024-10-16 18:47:10 +0800

    fix: 这样就不会打断渲染

--- src/form-design/Right.tsx
+++ src/form-design/Right.tsx
@@ -1,46 +1,46 @@
-import { FC } from 'react'
+import { Dispatch, FC, SetStateAction } from 'react'
 import Config from './config'
 import { IFormItemType } from './initData'
 
 type Props = {
   formItems: IFormItem[]
-  setFormItems: (items: IFormItem[]) => void
+  setFormItems: Dispatch<SetStateAction<IFormItem[]>>
   currId: string
 }
 const Right: FC<Props> = ({ formItems, setFormItems, currId }) => {
-  const RenderConfig = () => {
-    const item = formItems.find((item) => item.name === currId)
-    if (!item) {
-      return (
-        <>
-          <p>请选择表单项</p>
-        </>
-      )
-    }
-    switch (item.type) {
-      case IFormItemType.input:
-        return (
-          <Config.Input
-            formItems={formItems}
-            setFormItems={setFormItems}
-            currId={currId}
-          />
-        )
-      case IFormItemType.radio:
-        return (
-          <Config.Radio
-            formItems={formItems}
-            setFormItems={setFormItems}
-            currId={currId}
-          />
-        )
-    }
+  const item = formItems.find((item) => item.name === currId)
+  if (!item) {
+    return (
+      <div className='right flex flex-col gap-4 flex-basis-300px'>
+        <h3>表单配置</h3>
+        <p>请选择表单项</p>
+      </div>
+    )
   }
 
   return (
     <div className='right flex flex-col gap-4 flex-basis-300px'>
       <h3>表单配置</h3>
-      <RenderConfig />
+      {(() => {
+        switch (item.type) {
+          case IFormItemType.input:
+            return (
+              <Config.Input
+                formItems={formItems}
+                setFormItems={setFormItems}
+                currId={currId}
+              />
+            )
+          case IFormItemType.radio:
+            return (
+              <Config.Radio
+                formItems={formItems}
+                setFormItems={setFormItems}
+                currId={currId}
+              />
+            )
+        }
+      })()}
     </div>
   )
 }
