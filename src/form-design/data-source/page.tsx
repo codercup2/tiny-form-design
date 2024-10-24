89df9d9fea253e340676ce4fb879d18fa1b78bba
Burt <burt.lai@kupotech.com>
2024-10-24 21:45:14 +0800

    refine: page 数据

--- src/form-design/data-source/page.tsx
+++ src/form-design/data-source/page.tsx
@@ -2,13 +2,13 @@ import { deepClone } from '../utils'
 import { IComponentItemWithConsequenceId } from './helper'
 
 // 目前只有 '@kc/mk/market-page' 这种 pageLayout 组件
-export const rootId = 'root'
 // export const defaultPageLayoutType = '@kc/mk/market-page'
 export const defaultPageLayoutType = '@kc/lego-mk-ui/PlHeroTop'
 
 export const initState = {
   root: {
-    id: rootId,
+    // TODO: 如果不能左边拖拽过来，不需要放在左边，那么这个组件不需要有 G102 这样的id，那么是可以在这里指定一个的
+    id: '-1',
     // type: '@kc/mk/market-page',
     type: defaultPageLayoutType,
     theme: '@kc/mk/theme-xmas',
@@ -16,6 +16,7 @@ export const initState = {
     sceneProps: {
       taskCode: '',
     },
+    slots: [],
   },
   zones: {},
   locales: {},
@@ -26,40 +27,40 @@ export const initState = {
  */
 export function handleInitState(flatComps: IComponentItemWithConsequenceId[]) {
   const state = deepClone(initState)
-  const rootComp = flatComps.find((item) => item.name === defaultPageLayoutType)
+  const rootComp = flatComps.find(
+    (item) => item.name === defaultPageLayoutType
+  ) as any
+  if (!rootComp) {
+    console.error('root component not found')
+    return state
+  }
   state.root = {
     ...state.root,
     ...rootComp,
+    // id: rootId, // 这个根节点不能改了，否则拖拽进来找不到区域
   }
   console.log('handleInitState->', state)
   // 处理propss
-  // 通过 @kc/lego-mk-ui/PlHeroTop 拿到其他元信息
-  // 已经在 allComps 里面有，直接查找就行
-  // const allComps = getAllComponents() // TODO 异步数据获取
-  // const item = allComps.find((item) => item.name === '@kc/lego-mk-ui/PlHeroTop')
-  // if (typeof pageLayout.slots === 'undefined') {
-  //   state.root.slots = []
-  // } else if (typeof pageLayout.slots === 'boolean') {
-  //   if (pageLayout.slots) {
-  //     state.root.slots = ['children']
-  //   } else {
-  //     state.root.slots = []
-  //   }
-  // } else if (Array.isArray(pageLayout.slots)) {
-  //   state.root.slots = (pageLayout.slots || []).map((item) => {
-  //     if (typeof item === 'string') {
-  //       return item
-  //     }
-  //     if ('name' in item && typeof item.name === 'string') {
-  //       return item.name
-  //     }
-  //     console.error('invalid slot name', item)
-  //     return 'error-slot-name'
-  //   })
-  // }
+  // 通过 state.root.type 即 defaultPageLayoutType 拿到其他元信息
+  if (typeof rootComp.slots === 'undefined') {
+    state.root.slots = []
+  } else if (typeof rootComp.slots === 'boolean') {
+    if (rootComp.slots) {
+      state.root.slots = ['children'] as any
+    } else {
+      state.root.slots = []
+    }
+  } else if (Array.isArray(rootComp.slots)) {
+    state.root.slots = rootComp.slots
+  }
   // 处理zones
-  // ;(state.root.slots as string[]).forEach((slot) => {
-  //   state.zones[`${rootId}:${slot}`] = []
-  // })
+  ;(state.root.slots as string[]).forEach((slot) => {
+    const rootId = state.root.id // 经过上面的处理，这里已经不是最开始的
+    if (typeof slot === 'string') {
+      state.zones[`${rootId}:${slot}`] = []
+    } else {
+      state.zones[`${rootId}:${slot.name}`] = []
+    }
+  })
   return state
 }
