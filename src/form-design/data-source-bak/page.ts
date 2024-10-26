14fca6d1d892bafd9195d1041bc904261f801c38
Burt <burt.lai@kupotech.com>
2024-10-26 23:16:12 +0800

    refine

--- src/form-design/data-source-bak/page.ts
+++ src/form-design/data-source-bak/page.ts
@@ -100,14 +100,18 @@ export const someMarketPage: IPage = {
 
 // 经过协商，改为如下树状数据结构 2024-10-26 周六 晚23点
 const root = {
-  // 几个特别的根节点的属性
+  /** 几个特别的根节点的属性 */
+  // 多语言
   locales: {},
+  // 页面主题
   theme: '',
+  // 页面使用的业务场景
+  scene: '@kc/mk/scene-market',
   // 业务场景属性需要的业务属性配置
   sceneProps: {
     taskCode: 'xxxx',
   },
-  // 下面是通用的
+  /** 下面是通用的 */
   id: 'root',
   name: '@kc/mk/market-page',
   props: {},
